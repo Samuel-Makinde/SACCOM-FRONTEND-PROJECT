649e605bb986be77a174cd4b33f8f83a1eb8a1eb
baniex <banjibaniex1@gmail.com>
2023-10-27 12:09:08 +0100

    conditional rendering for properties with images and properties without

--- saccom/src/component/Home-Page/Products-Page/ProductViewPage/ProductView.jsx
+++ saccom/src/component/Home-Page/Products-Page/ProductViewPage/ProductView.jsx
@@ -466,7 +466,7 @@ const handlePrev = () => {
                 <h3 className="text-[#2F3035] text-[24px] font-semibold leading-[30.43px] md:text-[48px] md:leading-[60.82px]">
                   Description
                 </h3>
-                {/* <video
+                <video
                   className="h-4/5 mt-4 md:hidden"
                   style={{ objectFit: "cover", width: "100%", height: "40vh" }}
                   src={product.apartment_video}
@@ -474,7 +474,7 @@ const handlePrev = () => {
                   controls
                   muted
                   preload="metadata"
-                ></video> */}
+                ></video>
                 <p className="text-[#85868D] font-normal text-[16px] leading-[20.29px] mt-[10px]">
                   {product.apartment_full_description}
                 </p>
